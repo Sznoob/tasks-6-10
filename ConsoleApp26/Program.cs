8e72cd9d52619f7cb89dc46d0cdf4b1cb021b9ea
Joni Kananen <L7043@student.jamk.fi>
2018-01-14 12:38:50 +0200

    Tasks 6-10

--- ConsoleApp26/Program.cs
+++ ConsoleApp26/Program.cs
@@ -4,12 +4,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp26
+namespace Tasks
 {
-    class Program
+    class Programm
     {
         static void Main(string[] args)
         {
+
+            // task 7
+
+            Console.Write("Enter the Year in four digits: ");
+            try
+            {
+                int year = Convert.ToInt16(Console.ReadLine());
+
+                if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                    Console.WriteLine("Year is a Leap Year.");
+                else
+                    Console.WriteLine("Year is not a Leap Year. ");
+            }
+            catch
+            {
+                Console.WriteLine("Incorrect input");
+            }
+
+
+            //task 8
+
+            Console.WriteLine("Give a first number: ");
+            int a = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Give a second number: ");
+            int b = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Give a third number: ");
+            int c = Convert.ToInt16(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("The biggest number is: " + a);
+            }
+            if (b > a && b > c)
+            {
+                Console.WriteLine("The biggest number is: " + b);
+            }
+            if (c > a && c > b)
+            {
+                Console.WriteLine("The biggest number is: " + c);
+            }
+
+            int summary = 0;
+            int userValue;
+            do
+            {
+                Console.WriteLine("Give a number");
+
+                userValue = Convert.ToInt16(Console.ReadLine());
+                summary = summary + userValue;
+                Console.WriteLine("Summary is " + summary);
+            }
+            while (userValue != 0);
+
+
+            //task 10
+            int[] values = { 1, 2, 33, 44, 55, 68, 77, 96, 100 };
+            foreach (var result in values)
+            {
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine("HEP! ");
+                }
+                else
+                {
+                    Console.WriteLine("Value isn't even. ");
+                }
+
+            }
+
+
+
+
+
+
+            Console.Read();
         }
     }
 }
